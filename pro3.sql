219d807a60afdf1ff3d19f5b591ce8ad05caf403
Max Gu <guxinchi2000@gmail.com>
2014-06-06 23:20:34 -0700

    update pro3.sql

--- pro3.sql
+++ pro3.sql
@@ -1,10 +1,39 @@
-﻿
+﻿/*
+drop table sta_cat;
+drop table sta_pro;
+drop table cus_cat;
+drop table cus_pro;
+*/
+
+/* state | category | amount */
+create table sta_cat as (select u.state, p.category, SUM(o.price*o.quantity) as amount
+from user_t u, order_t o, product p
+where o.username = u.id
+and o.product = p.id
+group by u.state, p.category
+order by amount desc
+);
+
+
+/* state | product | amount */
+create table sta_pro as (select u.state, o.product, SUM(o.price*o.quantity) as amount
+from user_t u, order_t o
+where o.username = u.id
+group by u.state, o.product
+order by amount desc
+);
+
+
+/* uid | state | uname | product | amount  */
+
 create table cus_pro as (select u.id,u.state, u.name as username, p.name, SUM(o.price*o.quantity) as amount
 from user_t u, product p, order_t o
 where o.username = u.id 
 and   o.product = p.id
 group by u.id, p.id
 order by amount desc);
+
+/* uid | uname | category | state | amount */
 
 create table cus_cat as (select u.id,u.name, p.category, u.state,  SUM(o.price*o.quantity) as amount
 from user_t u, product p, order_t o
@@ -13,6 +42,7 @@ and   o.product = p.id
 group by u.id, p.category
 order by amount desc);
 
+/* state - get row header(top 20 product) */
 
 
 create index cus_pro_uid_idx ON cus_pro (id);
@@ -24,5 +54,14 @@ create index cus_cat_uid_idx ON cus_cat(id);
 create index cus_cat_cid_idx ON cus_cat(category);
 create index cus_cat_state_idx ON cus_cat(state);
 create index cus_cat_amount_idx ON cus_cat(amount);
+
+create index sta_cat_state_idx ON sta_cat(state);
+create index sta_cat_cid_idx ON sta_cat(category);
+create index sta_cat_amount_idx ON sta_cat(amount);
+
+create index sta_pro_state_idx ON sta_pro(state);
+create index sta_pro_pid_idx ON sta_pro(product);
+create index sta_pro_amount_idx ON sta_pro(amount);
+
 
 
